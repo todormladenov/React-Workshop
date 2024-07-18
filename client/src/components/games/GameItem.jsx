ccec19d88f9a7d705773b029a23c970435af32ba
Todor Mladenov <toshko958@gmail.com>
2024-07-18 17:22:49 +0200

    Created GameItem component

---
+++ client/src/components/games/GameItem.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function GameItem({ game }) {
+    return (
+        <div className="allGames">
+
+            <div className="allGames-info">
+                <img src={game.imageUrl} />
+                <h6>{game.category}</h6>
+                <h2>{game.title}</h2>
+                <Link to={`/games/${game._id}`} className="details-button">Details</Link>
+            </div>
+        </div>
+    );
+}
