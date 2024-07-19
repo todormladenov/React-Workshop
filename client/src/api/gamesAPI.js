10d32acf938e998d00f2874b5e21c0f59605a12b
Todor Mladenov <toshko958@gmail.com>
2024-07-19 11:57:08 +0200

    Update GameDetails to ender game dynamically

--- client/src/api/gamesAPI.js
+++ client/src/api/gamesAPI.js
@@ -3,3 +3,4 @@ import { get } from "./api"
 const baseURL = 'http://localhost:3030/data/games'
 
 export const getAllGames = async () => get(baseURL);
+export const getGameById = async (gameId) => get(`${baseURL}/${gameId}`);
--- client/src/components/games/GameDetails.jsx
+++ client/src/components/games/GameDetails.jsx
@@ -1,40 +1,51 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { getGameById } from "../../api/gamesAPI";
 
 export default function GameDetails() {
+    const [game, setGame] = useState({});
+    const [comments, setComments] = useState([]);
+
+    const { gameId } = useParams();
+
+    useEffect(() => {
+        (async () => {
+            const game = await getGameById(gameId);
+
+            setGame(game);
+        })()
+    }, []);
+
     return (
         <section id="game-details">
             <h1>Game Details</h1>
             <div className="info-section">
 
                 <div className="game-header">
-                    <img className="game-img" src="images/MineCraft.png" />
-                    <h1>Bright</h1>
-                    <span className="levels">MaxLevel: 4</span>
-                    <p className="type">Action, Crime, Fantasy</p>
+                    <img className="game-img" src={game.imageUrl} />
+                    <h1>{game.title}</h1>
+                    <span className="levels">MaxLevel: {game.maxLevel}</span>
+                    <p className="type">{game.category}y</p>
                 </div>
 
-                <p className="text">
-                    Set in Link world where fantasy creatures live side by side with humans. A human cop is forced to work
-                    with an Orc to find Link weapon everyone is prepared to kill for. Set in Link world where fantasy
-                    creatures live side by side with humans. A human cop is forced
-                    to work with an Orc to find Link weapon everyone is prepared to kill for.
-                </p>
+                <p className="text">{game.summary}</p>
 
                 <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
+                        {comments.map(comment =>
+                            <li key={comment._id} className="comment">
+                                <p>Content: {comment.comment}</p>
+                            </li>
+                        )}
                     </ul>
-                    <p className="no-comment">No comments.</p>
+
+                    {!comments.length && <p className="no-comment">No comments.</p>}
+
                 </div>
 
                 <div className="buttons">
-                    <Link to="/games/gameId/edit" className="button">Edit</Link>
+                    <Link to={`/games/${game._id}/edit`} className="button">Edit</Link>
                     <button className="button">Delete</button>
                 </div>
             </div>
