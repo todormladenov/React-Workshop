48f5ceb84fcd9483346ac12668d775da1f12cb07
Todor Mladenov <toshko958@gmail.com>
2024-07-25 13:37:23 +0200

    Add register functionality

--- client/src/api/authAPI.js
+++ client/src/api/authAPI.js
@@ -1,5 +1,7 @@
 import { post } from "./api"
 
-const baseURL = 'http://localhost:3030/users/login'
+const baseURL = 'http://localhost:3030/users'
 
-export const login = (email, password) => post(baseURL, { email, password });
+export const login = (email, password) => post(`${baseURL}/login`, { email, password });
+
+export const register = (email, password) => post(`${baseURL}/register`, { email, password });
--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,21 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { register } from "../../api/authAPI";
+import { useState } from "react";
 
 export default function Register() {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        'confirm-password': '',
+    });
+    const navigator = useNavigate();
+
+
+    const changeHandler = (e) => {
+        setValues(oldValues => ({
+            ...oldValues,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const registerHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+            const user = await register(values.email, values.password);
+            navigator('/');
+        } catch (error) {
+            console.error(error.message) //TODO: Implement error handling.
+        }
+    }
+
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" onSubmit={registerHandler}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
 
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com" />
+                    <input type="email" id="email" name="email" placeholder="maria@email.com" value={values.email} onChange={changeHandler} />
 
                     <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password" />
+                    <input type="password" name="password" id="register-password" value={values.password} onChange={changeHandler} />
 
                     <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password" />
+                    <input type="password" name="confirm-password" id="confirm-password" value={values['confirm-password']} onChange={changeHandler} />
 
                     <input className="btn submit" type="submit" value="Register" />
 
